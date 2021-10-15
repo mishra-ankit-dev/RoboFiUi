efcc7d20d6f8b7aa90429335eedf5a560cb28c68
Ankit Mishra <amishm766@gmail.com>
2021-10-15 12:24:13 +0530

    Removed redundant pages/folders.

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 //Install express server
-const express = require("express");
-const path = require("path");
-const compression = require("compression");
+const express = require('express');
+const path = require('path');
+const compression = require('compression');
 const app = express();
 app.use(compression());
 
 // app.use(express.compress());
 
 // Serve only the static files form the dist directory
-app.use(express.static("./dist/robofi"));
+app.use(express.static('./dist/robofi'));
 
-app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname, "/dist/robofi/index.html"));
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, '/dist/robofi/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 4201);
+app.listen(process.env.PORT || 4200);
